7dd7bf33cad41f5b3622e94d18bcee75121347d0
SivaranjiniSanthanakrishnan <43466009+SivaranjiniSanthanakrishnan@users.noreply.github.com>
2022-09-18 12:13:43 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./css/sb-admin-2.min.css";
+import "./vendor/fontawesome-free/css/all.min.css";
+import SideBar from "./components/sidebar";
+import Dashboard from "./components/dashboard";
+import Button from "./components/button";
+import Cards from "./components/cards";
+import UtilitiesColor from "./components/utilities-color";
+import UtilitiesBorder from "./components/utilities-border";
+import UtilitiesAnimation from "./components/utilities-animation";
+import Other from "./components/other";
+import Login from "./components/login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  let arr = [1, 2, 3, 4, 5];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {arr.map((ele) => ele * 5)}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <BrowserRouter>
+        <SideBar />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/button" element={<Button />} />
+          <Route path="/cards" element={<Cards />} />
+          <Route path="/color" element={<UtilitiesColor />} />
+          <Route path="/borders" element={<UtilitiesBorder />} />
+          <Route path="/animations" element={<UtilitiesAnimation />} />
+          <Route path="/other" element={<Other />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
